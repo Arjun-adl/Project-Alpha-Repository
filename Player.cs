ab00b3df63060d00930833740ee3520fd470da60
USERNAME <dyanidorff@gmail.com>
2026-09-15 13:43:39 +0200

    Added a inventory to the player class. And methods to print the items and add them

--- Player.cs
+++ Player.cs
@@ -2,11 +2,33 @@
 {
     public Location PlayerLocation;
     public readonly string Name;
+    public Dictionary<string, int> inventory;
 
     public Player(string name, Location location)
     {
         Name = name;
+        inventory = new Dictionary<string, int>();
         PlayerLocation = location;
     }
 
+    public void AddItem(string itemName, int amount = 1)
+    {
+        if (inventory.ContainsKey(itemName))
+        {
+            inventory[itemName] += amount;
+        }
+        else
+        {
+            inventory[itemName] = amount;
+        }
+    }
+
+    public void PrintInventory()
+    {   
+        Console.WriteLine("Inventory: ");
+        foreach (var item in inventory)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
 }
